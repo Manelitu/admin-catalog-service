b76c562c1cb3e0c36ed131519cd77b0c3b028293
Manelito <emanuelwork1@gmail.com>
2023-09-23 16:53:30 -0300

    feat: create main on infra module

--- infrastructure/src/main/java/org/admin/catalog/infrastructure/Main.java
+++ infrastructure/src/main/java/org/admin/catalog/infrastructure/Main.java
@@ -1,7 +1,10 @@
 package org.admin.catalog.infrastructure;
 
+import org.admin.catalog.application.UseCase;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(new UseCase().execute());
     }
 }
