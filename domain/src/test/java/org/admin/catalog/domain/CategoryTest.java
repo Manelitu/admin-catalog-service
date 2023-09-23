242d3bef9c8c73b51f85bd0b0a0875f5878958ba
Manelito <emanuelwork1@gmail.com>
2023-09-23 16:52:30 -0300

    test: create category on domain

---
+++ domain/src/test/java/org/admin/catalog/domain/CategoryTest.java
@@ -0,0 +1,11 @@
+package org.admin.catalog.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CategoryTest {
+    @Test
+    public void testNewCategory() {
+        Assertions.assertNotNull(new Category());
+    }
+}
