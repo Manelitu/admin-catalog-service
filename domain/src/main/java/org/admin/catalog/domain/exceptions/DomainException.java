ac8a20e17fbb0c3aef6c1579bfc106652afebdd0
Manelito <emanuelwork1@gmail.com>
2023-09-24 17:53:02 -0300

    refactor: extends domain exception by no stacktrace

--- domain/src/main/java/org/admin/catalog/domain/exceptions/DomainException.java
+++ domain/src/main/java/org/admin/catalog/domain/exceptions/DomainException.java
@@ -4,16 +4,16 @@ import org.admin.catalog.domain.validation.Error;
 
 import java.util.List;
 
-public class DomainException extends RuntimeException {
+public class DomainException extends NoStacktraceException {
     private final List<Error> errors;
 
-    private DomainException(final List<Error> errors) {
-        super("", null, true, false);
+    private DomainException(final String message, final List<Error> errors) {
+        super(message);
         this.errors = errors;
     }
 
-    public static DomainException with(final List<Error> errors) {
-        return new DomainException(errors);
+    public static DomainException with(final Error errors) {
+    return new DomainException(errors.message(), List.of(errors));
     }
 
     public List<Error> getErrors() {
