39f2fe82c75ba0679701287205afa48e59f1347b
Manelito <emanuelwork1@gmail.com>
2023-09-24 18:14:49 -0300

    refactor: adding final constant value with clean code

--- domain/src/main/java/org/admin/catalog/domain/validation/CategoryValidator.java
+++ domain/src/main/java/org/admin/catalog/domain/validation/CategoryValidator.java
@@ -3,6 +3,8 @@ package org.admin.catalog.domain.validation;
 import org.admin.catalog.domain.category.Category;
 
 public class CategoryValidator extends Validator {
+    public static final int NAME_MAX_LENGTH = 255;
+    public static final int NAME_MIN_LENGTH = 3;
     private final Category category;
 
     public CategoryValidator(final Category category, final ValidationHandler handler){
@@ -29,7 +31,7 @@ public class CategoryValidator extends Validator {
         }
 
         final int length = name.trim().length();
-        if (length > 255 || length < 3) {
+        if (length > NAME_MAX_LENGTH || length < NAME_MIN_LENGTH) {
             this.validationHandler().append(new Error("'name' must be have between 3 and 255 characters"));
         }
     }
