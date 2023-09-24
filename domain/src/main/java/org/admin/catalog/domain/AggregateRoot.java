7ea7bfe43af0dbe6a87e00dfa56709bd091e0a9a
Manelito <emanuelwork1@gmail.com>
2023-09-24 09:42:29 -0300

    feat: overriding validate on AggregateRoot

--- domain/src/main/java/org/admin/catalog/domain/AggregateRoot.java
+++ domain/src/main/java/org/admin/catalog/domain/AggregateRoot.java
@@ -1,7 +1,14 @@
 package org.admin.catalog.domain;
 
+import org.admin.catalog.domain.validation.ValidationHandler;
+
 public class AggregateRoot<ID extends Identifier> extends Entity<ID> {
     protected AggregateRoot(final ID id) {
         super(id);
+    }
+
+    @Override
+    public void validate(ValidationHandler handler) {
+
     }
 }
