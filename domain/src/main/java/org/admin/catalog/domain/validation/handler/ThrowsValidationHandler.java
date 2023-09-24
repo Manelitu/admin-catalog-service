f9a1cff0ff5d7f06528e5206f9a9c5fa6b352b1a
Manelito <emanuelwork1@gmail.com>
2023-09-24 09:42:47 -0300

    feat: create ThrowsValidationHandler

---
+++ domain/src/main/java/org/admin/catalog/domain/validation/handler/ThrowsValidationHandler.java
@@ -0,0 +1,41 @@
+package org.admin.catalog.domain.validation.handler;
+
+import org.admin.catalog.domain.exceptions.DomainException;
+import org.admin.catalog.domain.validation.Error;
+import org.admin.catalog.domain.validation.ValidationHandler;
+
+import java.util.List;
+
+public class ThrowsValidationHandler implements ValidationHandler {
+
+    @Override
+    public ValidationHandler append(final Error error) {
+        throw DomainException.with(List.of(error));
+    }
+
+    @Override
+    public ValidationHandler append(final ValidationHandler handler) {
+        throw DomainException.with(handler.getErrors());
+    }
+
+    @Override
+    public ValidationHandler append(final Validation validation) {
+        try {
+            validation.validate();
+        } catch (final Exception ex) {
+            throw DomainException.with(List.of(new Error(ex.getMessage())));
+        }
+
+        return this;
+    }
+
+    @Override
+    public List<Error> getErrors() {
+        return List.of();
+    }
+
+    @Override
+    public boolean hasError() {
+        return ValidationHandler.super.hasError();
+    }
+}
