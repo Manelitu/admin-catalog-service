697c7ec9f3ff8f4267c416c0387ad412da73f2c3
Manelito <emanuelwork1@gmail.com>
2023-09-24 17:53:21 -0300

    refactor: replacing list of errors

--- domain/src/main/java/org/admin/catalog/domain/validation/handler/ThrowsValidationHandler.java
+++ domain/src/main/java/org/admin/catalog/domain/validation/handler/ThrowsValidationHandler.java
@@ -4,13 +4,12 @@ import org.admin.catalog.domain.exceptions.DomainException;
 import org.admin.catalog.domain.validation.Error;
 import org.admin.catalog.domain.validation.ValidationHandler;
 
-import java.util.List;
 
 public class ThrowsValidationHandler implements ValidationHandler {
 
     @Override
     public ValidationHandler append(final Error error) {
-        throw DomainException.with(List.of(error));
+        throw DomainException.with(error);
     }
 
     @Override
@@ -23,15 +22,15 @@ public class ThrowsValidationHandler implements ValidationHandler {
         try {
             validation.validate();
         } catch (final Exception ex) {
-            throw DomainException.with(List.of(new Error(ex.getMessage())));
+            throw DomainException.with(new Error(ex.getMessage()));
         }
 
         return this;
     }
 
     @Override
-    public List<Error> getErrors() {
-        return List.of();
+    public Error getErrors() {
+        return new Error("");
     }
 
     @Override
