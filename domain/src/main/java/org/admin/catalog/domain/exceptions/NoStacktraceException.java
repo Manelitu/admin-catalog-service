2d5e111bda994d90d8847e4f1d14e66cf5cc65a9
Manelito <emanuelwork1@gmail.com>
2023-09-24 17:44:29 -0300

    refactor: create NoStacktrace Exception

---
+++ domain/src/main/java/org/admin/catalog/domain/exceptions/NoStacktraceException.java
@@ -0,0 +1,11 @@
+package org.admin.catalog.domain.exceptions;
+
+public class NoStacktraceException extends RuntimeException {
+    public NoStacktraceException(final String message) {
+       this(message, null);
+    }
+
+    public NoStacktraceException(final String message, final Throwable cause) {
+        super(message, cause, true, false);
+    }
+}
