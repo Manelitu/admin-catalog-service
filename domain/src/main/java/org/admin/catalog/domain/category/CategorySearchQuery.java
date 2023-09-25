0b1b0413647933d3c144ed29999a3b856d9bfd96
Manelito <emanuelwork1@gmail.com>
2023-09-25 08:46:17 -0300

    feat: create search query of category

---
+++ domain/src/main/java/org/admin/catalog/domain/category/CategorySearchQuery.java
@@ -0,0 +1,10 @@
+package org.admin.catalog.domain.category;
+
+public record CategorySearchQuery(
+        int page,
+        int perPage,
+        String terms,
+        String sort,
+        String direction
+    ) {
+}
