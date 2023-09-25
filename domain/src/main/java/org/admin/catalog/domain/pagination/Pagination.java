aa4c9d6abb07fda3740573f40c0c060f09206efd
Manelito <emanuelwork1@gmail.com>
2023-09-25 08:46:00 -0300

    feat: create generic pagination record

---
+++ domain/src/main/java/org/admin/catalog/domain/pagination/Pagination.java
@@ -0,0 +1,11 @@
+package org.admin.catalog.domain.pagination;
+
+import java.util.List;
+
+public record Pagination<T>(
+        int currentPage,
+        int perPage,
+        long total,
+        List<T> items
+        ) {
+}
