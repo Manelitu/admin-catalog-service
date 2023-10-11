dd80d1d68c1c77736cdf689014a2a5880171cfdb
Manelito <emanuelwork1@gmail.com>
2023-10-11 20:12:43 -0300

    test: create a new category use case with valid command

---
+++ application/src/test/java/org/admin/catalog/application/category/create/CreateCategoryUseCaseTest.java
@@ -0,0 +1,48 @@
+package org.admin.catalog.application.category.create;
+
+import org.admin.catalog.domain.category.CategoryGateway;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Objects;
+
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+
+public class CreateCategoryUseCaseTest {
+    @Test
+    public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() {
+        final var expectedName = "Any movie";
+        final var expectedDescription = "Any description";
+        final var expectedIsActive = true;
+
+        final var command = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        final CategoryGateway categoryGateway = Mockito.mock(CategoryGateway.class);
+        Mockito
+                .when(categoryGateway.create(Mockito.any()))
+                .thenAnswer(returnsFirstArg());
+
+        final var useCase = new DefaultCreateCategoryUseCase(categoryGateway);
+
+        final var actualOutput = useCase.execute(command);
+
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertNotNull(actualOutput.id());
+
+        Mockito.verify(categoryGateway, Mockito.times(1))
+                .create(Mockito.argThat(category -> {
+                    return Objects.equals(expectedName, category.getName())
+                            && Objects.equals(expectedDescription, category.getDescription())
+                            && Objects.equals(expectedIsActive, category.getActive())
+                            && Objects.nonNull(category.getId())
+                            && Objects.nonNull(category.getCreatedAt())
+                            && Objects.nonNull(category.getUpdatedAt())
+                            && Objects.isNull(category.getDeletedAt());
+                }));
+    }
+
+    // 2. Teste passando uma propriedade inválida
+    // 3. Teste criando uma categoria inativa
+    // 4. Teste Simulando um erro generico vindo do gateway
+}
