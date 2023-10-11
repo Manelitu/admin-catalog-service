7fe7d55325c746fde19f0462e3de425bfac1d8f9
Manelito <emanuelwork1@gmail.com>
2023-10-11 20:12:02 -0300

    feat: create category abstract use case

---
+++ application/src/main/java/org/admin/catalog/application/category/create/CreateCategoryUseCase.java
@@ -0,0 +1,6 @@
+package org.admin.catalog.application.category.create;
+
+import org.admin.catalog.application.UseCase;
+
+public abstract class CreateCategoryUseCase
+        extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {}
